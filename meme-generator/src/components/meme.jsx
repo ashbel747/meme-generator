37b7b8636c21a6f09a2dde20eb7bc84001909f3f
Ashbel <kashbel747@gmail.com>
2025-07-03 14:28:31 +0300

    Refactor: Acessibility changes

--- meme-generator/src/components/meme.jsx
+++ meme-generator/src/components/meme.jsx
@@ -4,16 +4,24 @@ export default function Meme() {
     return (
         <main>
             <form className="form">
-                <input 
+                <div>
+                    <label htmlFor="top-text">Top Text</label>
+                    <input 
+                    id="top-text"
                     type="text"
-                    placeholder="Top text"
+                    placeholder="Shut up"
                     className="form--input"
-                />
-                <input 
+                    />
+                </div>
+                <div>
+                    <label htmlFor="bottom-text">Bottom Text</label>
+                    <input 
+                    id="bottom-text"
                     type="text"
-                    placeholder="Bottom text"
+                    placeholder="and take my money"
                     className="form--input"
-                />
+                    />
+                </div>
                 <button 
                     className="form--button"
                 >
