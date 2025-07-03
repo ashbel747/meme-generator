e995454374e682cc090524949663343f9073b525
Ashbel <kashbel747@gmail.com>
2025-07-03 13:07:18 +0300

    Initial commit: Create the static components

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,16 @@
+import './app.css'
+import Navbar from "./components/navbar"
+import Meme from './components/meme'
+
+
+function App() {
+        
+  return (
+    <>
+      <Navbar />
+      <Meme  />
+    </>
+  )
+}
+
+export default App
---
+++ meme-generator/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+export default function Navbar() {
+    return (
+        <header className="header">
+            <img src="../src/assets/troll-face.png" className='header--image' />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
