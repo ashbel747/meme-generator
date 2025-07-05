6decdd45acdc052231bab33f96dae83ec27a41c4
Ashbel <kashbel747@gmail.com>
2025-07-05 12:40:40 +0300

    Complete section: Get memes from API

--- meme-generator/src/components/meme.jsx
+++ meme-generator/src/components/meme.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import '../App.css'
-import memesData from "../memesData"
+
 
 export default function Meme() {
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
-        randomImage: "" 
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = React.useState([])
     
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     
     function getMemeImage(event) {
         event.preventDefault();
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
+        
     }
-
+    
     function handleChange(event) {
         const {name, value} = event.target
         setMeme(prevMeme => ({
@@ -29,7 +34,7 @@ export default function Meme() {
             [name]: value
         }))
     }
-
+    
     return (
         <main>
             <form className="form">
